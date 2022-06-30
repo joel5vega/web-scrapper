700ffc7d1d3d510a965d9435c3bde8672d8aeebc
joel5vega <joel5vega@gmail.com>
2022-06-30 07:24:06 -0400

    get enterprises

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
-
+url = 'https://directoriodecarga.com/bolivia/?&sector=&pais=BO&clave=&pag='
 with open  ('home.html','r') as html_file:
     content =html_file.read()
     
     soup = BeautifulSoup(content, 'lxml')
     # print(soup.prettify())
-    empresas = soup.find_all('h2')
-
+    # empresas = soup.find_all('h2')
+    empresas = soup.find_all('a',class_="tooltip-1")
     for empresa in empresas:
+        print(empresa.ref)
         print(empresa.text.strip())
     
